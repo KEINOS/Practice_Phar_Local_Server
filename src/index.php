2aa96bd1f51b7b1f7917d89244d254e1381c0e8b
KEINOS <github@keinos.com>
2018-02-03 10:56:39 +0900

    feat: Add constants file

--- src/index.php
+++ src/index.php
@@ -1,4 +1,15 @@
 <?php
-include_once('function.php.inc');
+error_reporting(E_ALL ^ E_NOTICE);
+include_once('constants.php.inc');
+include_once('functions.php.inc');
 
-echo_hello();
+//echo_hello();
+
+if(set_dir_template('template')){
+    $path_file_tpl_index =  get_dir_template() . 'index.tpl';   
+    if( file_exists($path_file_tpl_index) ){
+        header("Content-Type: text/html; charset=UTF-8");
+        include($path_file_tpl_index);
+    }
+}
+
